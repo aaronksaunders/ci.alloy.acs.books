a63dd7fbd09cbded6757c2164a6ad5fe47b4381d
Aaron Saunders <aaron@clearlyinnovative.com>
2014-08-17 15:25:55 -0400

    updated application to work with latest titanium version and fixed the update issue

--- app/assets/alloy/sync/acs.js
+++ app/assets/alloy/sync/acs.js
@@ -1,187 +1,197 @@
 var STR = require("alloy/string");
 
 function S4() {
-    return ((1 + Math.random()) * 65536 | 0).toString(16).substring(1);
+	return ((1 + Math.random()) * 65536 | 0).toString(16).substring(1);
 }
 
 function guid() {
-    return S4() + S4() + "-" + S4() + "-" + S4() + "-" + S4() + "-" + S4() + S4() + S4();
+	return S4() + S4() + "-" + S4() + "-" + S4() + "-" + S4() + "-" + S4() + S4() + S4();
 }
 
 function InitAdapter(config) {
-    Cloud = require("ti.cloud"), Cloud.debug = !0, config.Cloud = Cloud;
+	Cloud = require("ti.cloud"), Cloud.debug = !0, config.Cloud = Cloud;
 }
 
 function Sync(method, model, opts) {
 
-    var object_name = model.config.adapter.collection_name;
-    var isCustomObject = model.config.adapter.custom;
-    var object_method = isCustomObject ? Cloud['Objects'] : Cloud[STR.ucfirst(object_name)];
-    object_name = isCustomObject ? object_name.replace(/s$/i, '') : object_name;
+	var object_name = model.config.adapter.collection_name;
+	var isCustomObject = model.config.adapter.custom;
+	var object_method = isCustomObject ? Cloud['Objects'] : Cloud[STR.ucfirst(object_name)];
+	object_name = isCustomObject ? object_name.replace(/s$/i, '') : object_name;
 
-    Ti.API.info("method " + method); debugger;
+	Ti.API.info("method " + method);
+	debugger;
 
-    switch (method) {
-        case "create":
-            // stick attributes into the params variable
-            var params = {};
-            // if custom object then set the classname in params variable
-            if (isCustomObject === true) {
-                params['fields'] = model.toJSON();
-                params['classname'] = object_name;
-            } else {
-                params = model.toJSON();
-            }
-            object_method.create(params, function(e) {
-                if (e.success) {
-                    model.meta = e.meta;
-                    opts.success && opts.success(e[object_name][0]), model.trigger("fetch");
-                    return;
-                }
-                Ti.API.error(e);
-                opts.error && opts.error(e.error && e.message || e);
-            });
-            break;
-        case "read":
-            var id_name = object_name.replace(/s+$/, "") + "_id", params = {};
-            params[id_name] = model.id = opts.id || model.id;
+	switch (method) {
+		case "create":
+			// stick attributes into the params variable
+			var params = {};
+			// if custom object then set the classname in params variable
+			if (isCustomObject === true) {
+				params['fields'] = model.toJSON();
+				params['classname'] = object_name;
+			} else {
+				params = model.toJSON();
+			}
+			object_method.create(params, function(e) {
+				if (e.success) {
+					model.meta = e.meta;
+					opts.success && opts.success(e[object_name][0]), model.trigger("fetch");
+					return;
+				}
+				Ti.API.error(e);
+				opts.error && opts.error(e.error && e.message || e);
+			});
+			break;
+		case "read":
+			var id_name = object_name.replace(/s+$/, "") + "_id", params = {};
+			params[id_name] = model.id = opts.id || model.id;
 
-            if (isCustomObject === true) {
-                !opts.data ? opts.data = {} : opts.data;
-                opts.data['classname'] = object_name;
-                opts.data['id'] = model.id;
-            } else {
-                id_name = object_name.replace(/s+$/, "") + "_id";
-                model.id && (opts.data[id_name] = model.id);
-            }
+			if (isCustomObject === true) {
+				!opts.data ? opts.data = {} : opts.data;
+				opts.data['classname'] = object_name;
+				opts.data['id'] = model.id;
+			} else {
+				id_name = object_name.replace(/s+$/, "") + "_id";
+				model.id && (opts.data[id_name] = model.id);
+			}
 
-            if (model.id) {
-                getObject(model, opts);
-            } else if (opts && opts.data && opts.data.q) {
-                searchObjects(model, opts);
-            } else {
-                getObjects(model, opts);
-            }
-            break;
-        case "update":
-            Ti.API.info(' updating object with id ' + model.id);
+			if (model.id) {
+				getObject(model, opts);
+			} else if (opts && opts.data && opts.data.q) {
+				searchObjects(model, opts);
+			} else {
+				getObjects(model, opts);
+			}
+			break;
+		case "update":
+			Ti.API.info(' updating object with id ' + model.id);
 
-            var params = model.toJSON(), id_name = object_name.replace(/s+$/, "") + "_id";
-            object_method.update(params, function(e) {
-                if (e.success) {
-                    model.meta = e.meta;
-                    opts.success && opts.success(e[object_name][0]), model.trigger("fetch");
-                    return;
-                }
-                Ti.API.error(e);
-                opts.error && opts.error(e.error && e.message || e);
-            }), model.trigger("fetch");
-            break;
-        case "delete":
-            var id_name = "";
-            var params = {};
+			var opt = {}, params = model.toJSON(), id_name = object_name.replace(/s+$/, "") + "_id";
+			if (model.config.adapter.custom === !0) {
+				opt.classname = object_name;
+				opt.id = model.id;
+				opt.fields = JSON.stringify(model.toJSON());
+				params = opt;
+			} else {
+				id_name = object_name.replace(/s+$/, "") + "_id";
+				params[id_name] = model.id;
+			}
+			object_method.update(params, function(e) {
+				if (e.success) {
+					model.meta = e.meta;
+					opts.success && opts.success(e[object_name][0]), model.trigger("fetch");
+					return;
+				}
+				Ti.API.error(e);
+				opts.error && opts.error(e.error && e.message || e);
+			}), model.trigger("fetch");
+			break;
+		case "delete":
+			var id_name = "";
+			var params = {};
 
-            if (model.config.adapter.custom === true) {
-                params['classname'] = object_name;
-                params['id'] = model.id;
-            } else {
-                id_name = object_name.replace(/s+$/, "") + "_id";
-                params[id_name] = model.id;
-            }
+			if (model.config.adapter.custom === true) {
+				params['classname'] = object_name;
+				params['id'] = model.id;
+			} else {
+				id_name = object_name.replace(/s+$/, "") + "_id";
+				params[id_name] = model.id;
+			}
 
-            object_method.remove(params, function(e) {
-                if (e.success) {
-                    model.meta = e.meta;
-                    opts.success && opts.success({}), model.trigger("fetch");
-                    return;
-                }
-                Ti.API.error(e);
-                opts.error && opts.error(e.error && e.message || e);
-            });
-    }
+			object_method.remove(params, function(e) {
+				if (e.success) {
+					model.meta = e.meta;
+					opts.success && opts.success({}), model.trigger("fetch");
+					return;
+				}
+				Ti.API.error(e);
+				opts.error && opts.error(e.error && e.message || e);
+			});
+	}
 }
 
 function getObject(_model, _opts) {
-    var object_name = _model.config.adapter.collection_name;
-    var isCustomObject = _model.config.adapter.custom;
-    var object_method = isCustomObject ? Cloud['Objects'] : Cloud[STR.ucfirst(object_name)];
-    object_name = isCustomObject ? object_name.replace(/s$/i, '') : object_name;
+	var object_name = _model.config.adapter.collection_name;
+	var isCustomObject = _model.config.adapter.custom;
+	var object_method = isCustomObject ? Cloud['Objects'] : Cloud[STR.ucfirst(object_name)];
+	object_name = isCustomObject ? object_name.replace(/s$/i, '') : object_name;
 
-    Ti.API.info(" searching for object id " + JSON.stringify(_opts.data));
-    object_method.show(_opts.data, function(e) {
-        if (e.success) {
-            if (_model.id) {
-                _model.meta = e.meta;
-                _opts.success && _opts.success(e[object_name][0]), _model.trigger("fetch");
-                return;
-            }
-        } else {
-            Ti.API.error(e);
-            _opts.error && _opts.error(e.error && e.message || e);
-        }
-    });
+	Ti.API.info(" searching for object id " + JSON.stringify(_opts.data));
+	object_method.show(_opts.data, function(e) {
+		if (e.success) {
+			if (_model.id) {
+				_model.meta = e.meta;
+				_opts.success && _opts.success(e[object_name][0]), _model.trigger("fetch");
+				return;
+			}
+		} else {
+			Ti.API.error(e);
+			_opts.error && _opts.error(e.error && e.message || e);
+		}
+	});
 }
 
 function getObjects(_model, _opts) {
-    var object_name = _model.config.adapter.collection_name;
-    var isCustomObject = _model.config.adapter.custom;
-    var object_method = isCustomObject ? Cloud['Objects'] : Cloud[STR.ucfirst(object_name)];
-    object_name = isCustomObject ? object_name.replace(/s$/i, '') : object_name;
+	var object_name = _model.config.adapter.collection_name;
+	var isCustomObject = _model.config.adapter.custom;
+	var object_method = isCustomObject ? Cloud['Objects'] : Cloud[STR.ucfirst(object_name)];
+	object_name = isCustomObject ? object_name.replace(/s$/i, '') : object_name;
 
-    if (isCustomObject === true) {
-        !_opts.data ? _opts.data = {} : _opts.data;
-        _opts.data['classname'] = object_name;
-    }
+	if (isCustomObject === true) {
+		!_opts.data ? _opts.data = {} : _opts.data;
+		_opts.data['classname'] = object_name;
+	}
 
-    object_method.query((_opts.data || {}), function(e) {
-        if (e.success) {
-            var retArray = [];
-            for (var i in e[object_name]) {
-                retArray.push(e[object_name][i]);
-            }
-            _model.meta = e.meta;
-            _opts.success && _opts.success(retArray), _model.trigger("fetch");
-            return;
-        } else {
-            Ti.API.error(e);
-            _opts.error && _opts.error(e.error && e.message || e);
-        }
-    });
+	object_method.query((_opts.data || {}), function(e) {
+		if (e.success) {
+			var retArray = [];
+			for (var i in e[object_name]) {
+				retArray.push(e[object_name][i]);
+			}
+			_model.meta = e.meta;
+			_opts.success && _opts.success(retArray), _model.trigger("fetch");
+			return;
+		} else {
+			Ti.API.error(e);
+			_opts.error && _opts.error(e.error && e.message || e);
+		}
+	});
 }
 
 function searchObjects(_model, _opts) {
-    var object_name = _model.config.adapter.collection_name;
-    var isCustomObject = _model.config.adapter.custom;
-    var object_method = isCustomObject ? Cloud['Objects'] : Cloud[STR.ucfirst(object_name)];
-    object_name = isCustomObject ? object_name.replace(/s$/i, '') : object_name;
+	var object_name = _model.config.adapter.collection_name;
+	var isCustomObject = _model.config.adapter.custom;
+	var object_method = isCustomObject ? Cloud['Objects'] : Cloud[STR.ucfirst(object_name)];
+	object_name = isCustomObject ? object_name.replace(/s$/i, '') : object_name;
 
-    if (isCustomObject === true) {
-        !_opts.data ? _opts.data = {} : _opts.data;
-        _opts.data['classname'] = object_name;
-    }
+	if (isCustomObject === true) {
+		!_opts.data ? _opts.data = {} : _opts.data;
+		_opts.data['classname'] = object_name;
+	}
 
-    object_method.search(_opts.data, function(e) {
-        if (e.success) {
-            if (e[object_name].length !== 0) {
-                var retArray = [];
-                for (var i in e[object_name]) {
-                    retArray.push(e[object_name][i]);
-                }
-                _model.meta = e.meta;
-                _opts.success && _opts.success(retArray), _model.trigger("fetch");
-                return;
-            }
-        } else {
-            Ti.API.error(e);
-            _opts.error && _opts.error(e.error && e.message || e);
-        }
-    });
+	object_method.search(_opts.data, function(e) {
+		if (e.success) {
+			if (e[object_name].length !== 0) {
+				var retArray = [];
+				for (var i in e[object_name]) {
+					retArray.push(e[object_name][i]);
+				}
+				_model.meta = e.meta;
+				_opts.success && _opts.success(retArray), _model.trigger("fetch");
+				return;
+			}
+		} else {
+			Ti.API.error(e);
+			_opts.error && _opts.error(e.error && e.message || e);
+		}
+	});
 }
 
 var Cloud, _ = require("alloy/underscore")._;
 
 module.exports.sync = Sync, module.exports.beforeModelCreate = function(config) {
-    return config = config || {}, config.data = {}, InitAdapter(config), config;
+	return config = config || {}, config.data = {}, InitAdapter(config), config;
 }, module.exports.afterModelCreate = function(Model) {
-    return Model = Model || {}, Model.prototype.config.Model = Model, Model;
+	return Model = Model || {}, Model.prototype.config.Model = Model, Model;
 };
--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,110 +1,137 @@
 // Function to keep a Ti.TableView in sync with Backbone Model.
 $.table.updateContent = function(collection) {
-    var rows = [];
-    for (var i = 0; i < collection.length; i++) {
-        var model = collection.at(i).attributes, title = "";
-        for (var key in model) {
-            if (key !== "id") {
-                title += model[key] + "  "
-            }
-        }
-        rows.push(Ti.UI.createTableViewRow({
-            "title" : title
-        }));
-    }
-    this.setData(rows);
+	var rows = [];
+	for (var i = 0; i < collection.length; i++) {
+		var model = collection.at(i).attributes, title = "";
+		for (var key in model) {
+			if (key !== "id") {
+				title += model[key] + "  "
+			}
+		}
+		rows.push(Ti.UI.createTableViewRow({
+			"title" : title
+		}));
+	}
+	this.setData(rows);
 };
 
 function do_the_magic() {
-    // Now let's create a Backbone collection that will hold our models,
-    // the classes that represent our model have been generated automatically
-    // as Alloy components. Use new on the component to create the model or
-    // collection.
-    var books = Alloy.createCollection('Book');
+	// Now let's create a Backbone collection that will hold our models,
+	// the classes that represent our model have been generated automatically
+	// as Alloy components. Use new on the component to create the model or
+	// collection.
+	var books = Alloy.createCollection('Book');
 
-    // You can bind any Backbone event to models or collections but fetch is convenient because
-    // fetch occurs when the persistent store is sync'd to the local Backbone server.
-    books.on("fetch", function() {
-        $.table.updateContent(books);
-    });
+	// You can bind any Backbone event to models or collections but fetch is convenient because
+	// fetch occurs when the persistent store is sync'd to the local Backbone server.
+	books.on("fetch", function() {
+		$.table.updateContent(books);
+	});
 
-    // Fetch will load models from persistent storage, sync'ing Backbone and persistent store.
-    books.fetch();
+	// Fetch will load models from persistent storage, sync'ing Backbone and persistent store.
+	books.fetch();
 
-    // Now we can add items to the model.
-    var book = Alloy.createModel('Book', {
-        book : "Jungle Book",
-        author : "Kipling"
-    });
-    books.add(book);
+	// Now we can add items to the model.
+	var book = Alloy.createModel('Book', {
+		book : "Jungle Book",
+		author : "Kipling"
+	});
+	books.add(book);
 
-    // Use Backbone shortcut to create a model and add to collection in single step. Does the same
-    // thing as the creating a new model and then adding it to the collection.
-    books.add({
-        book : "War and Peace",
-        author : "Tolstoy"
-    });
+	// Use Backbone shortcut to create a model and add to collection in single step. Does the same
+	// thing as the creating a new model and then adding it to the collection.
+	books.add({
+		book : "War and Peace",
+		author : "Tolstoy"
+	});
 
-    // Add will add models to local Backbone server but save triggers the CRUD create operation
-    // causing the model to get added to the persistent store. During create an id is added to the
-    // model signaling that the model has been persisted and no longer in the new state.
-    books.forEach(function(model) {
-        model.save();
-    });
+	// Add will add models to local Backbone server but save triggers the CRUD create operation
+	// causing the model to get added to the persistent store. During create an id is added to the
+	// model signaling that the model has been persisted and no longer in the new state.
+	books.forEach(function(model) {
+		model.save();
+	});
 
-    // UPDATE - update the model save here triggers the CRUD update operation
-    book.save({
-        author : "R Kipling"
-    });
+	// UPDATE - update the model save here triggers the CRUD update operation
+	book.save({
+		author : "R Kipling"
+	});
 
-    // Okay time to show the results. Remember this sync's local Backbone server with persistent store.
-    books.fetch();
+	// Okay time to show the results. Remember this sync's local Backbone server with persistent store.
+	books.fetch();
 
-    // DELETE - destroy triggers the CRUD delete operation
-    for ( i = books.length - 1; i >= 0; i--) {
-        var model = books.at(i);
-        //model.destroy();
-    };
+	// DELETE - destroy triggers the CRUD delete operation
+	for ( i = books.length - 1; i >= 0; i--) {
+		var model = books.at(i);
+		//model.destroy();
+	};
 
-    aUser.destroy();
+	aUser.destroy();
 }
 
 //
 // CREATE THE USER
 //
 var options = {
-    success : function(_m, _r) {
-        Ti.API.info(' SUCCESS ' + JSON.stringify(_m));
-        Ti.API.info(' model stringified ' + _m.get("username"));
+	success : function(_m, _r) {
+		Ti.API.info(' SUCCESS ' + JSON.stringify(_m));
+		Ti.API.info(' model stringified ' + _m.get("username"));
 
-        Ti.API.info(' stored session ' + _m.retrieveStoredSession());
+		Ti.API.info(' stored session ' + _m.retrieveStoredSession());
 
-        do_the_magic();
-    },
-    error : function(_m, _e) {
-        Ti.API.info(' ERROR ' + JSON.stringify(_e));
+		// do_the_magic();
 
-        if (_e === "Username is already taken") {
-            loginUser();
-        }
+		// Create a book
+		var book = Alloy.createModel('Book', {
+			book : "Birth of A Nation",
+			author : "Saunders"
+		});
 
-    }
+		// save a book
+		book.save({}, {
+			success : function() {
+
+				// update the book
+				book.save({
+					author : "R Kipling"
+				}, {
+					success : function() {
+						debugger;
+					},
+					error : function() {
+						debugger;
+					}
+				});
+			},
+			error : function() {
+
+			}
+		});
+	},
+	error : function(_m, _e) {
+		Ti.API.info(' ERROR ' + JSON.stringify(_e));
+
+		if (_e.indexOf("Username is already taken") !== -1) {
+			loginUser();
+		}
+
+	}
 };
 
 function createUser() {
-    var aUser = Alloy.createModel('User', {
-        username : "testuser",
-        password : "password",
-        password_confirmation : "password",
-        first_name : "Aaron",
-        last_name : "Saunders"
-    });
-    aUser.save({}, options);
+	var aUser = Alloy.createModel('User', {
+		username : "testuser",
+		password : "password",
+		password_confirmation : "password",
+		first_name : "Aaron",
+		last_name : "Saunders"
+	});
+	aUser.save({}, options);
 }
 
 function loginUser() {
-    var aUser = Alloy.createModel('User');
-    aUser.login("testuser", "password", options);
+	var aUser = Alloy.createModel('User');
+	aUser.login("testuser", "password", options);
 }
 
 $.index.open();
